923cdf310df45c5a34db49e241bf0ae4c5e5bc53
Cosimo Matteini <dev.matteini@gmail.com>
2024-03-25 23:19:20 +0100

    schema: 05 json schema

---
+++ src/schema/05-json-schema.test.ts
@@ -0,0 +1,28 @@
+import { expect, test, describe } from "vitest"
+import * as S from "@effect/schema/Schema"
+import * as JSONSchema from "@effect/schema/JSONSchema"
+
+const Person = S.struct({
+    name: S.string,
+    age: S.NumberFromString,
+})
+
+test("generate JSON Schema", () => {
+    const personJsonSchema = JSONSchema.make(Person)
+
+    const result = JSON.stringify(personJsonSchema)
+
+    expect(result).toEqual(
+        '{"$schema":"http://json-schema.org/draft-07/schema#","type":"object","required":["name","age"],"properties":{"name":{"type":"string","description":"a string","title":"string"},"age":{"$ref":"#/$defs/NumberFromString"}},"additionalProperties":false,"$defs":{"NumberFromString":{"type":"number","description":"a number","title":"number"}}}',
+    )
+})
+
+test("generate JSON Schema from Schema.Encoded", () => {
+    const personEncodedJsonSchema = JSONSchema.make(S.encodedSchema(Person))
+
+    const result = JSON.stringify(personEncodedJsonSchema)
+
+    expect(result).toEqual(
+        '{"$schema":"http://json-schema.org/draft-07/schema#","type":"object","required":["name","age"],"properties":{"name":{"type":"string","description":"a string","title":"string"},"age":{"type":"string","description":"a string","title":"string"}},"additionalProperties":false}',
+    )
+})
