05c09850f9ee0c001d22b6b35fed5a5b86c09a51
Cosimo Matteini <dev.matteini@gmail.com>
2024-03-25 22:50:48 +0100

    schema: 03 assertions

---
+++ src/schema/03-assertions.test.ts
@@ -0,0 +1,28 @@
+import { expect, test, describe } from "vitest"
+import * as S from "@effect/schema/Schema"
+import * as AST from "@effect/schema/AST"
+import * as Effect from "effect/Effect"
+import * as EV from "@effect/vitest"
+import * as E from "effect/Either"
+
+const Person = S.struct({
+    name: S.string,
+    age: S.number,
+})
+
+const isPerson = S.is(Person)
+
+test("is person", () => {
+    expect(isPerson({ name: "Alice", age: 30 })).toBeTruthy()
+    expect(isPerson({ name: "Alice", age: "30" })).toBeFalsy()
+    expect(isPerson(null)).toBeFalsy()
+})
+
+// Typescript `asserts` keyword: https://stackoverflow.com/questions/71624824/what-does-the-typescript-asserts-operator-do
+const assertsPerson = S.asserts(Person)
+
+test("asserts", () => {
+    expect(() => assertsPerson({ name: "Alice", age: 30 })).not.toThrowError()
+    expect(() => assertsPerson({ name: "Alice", age: "30" })).toThrowError()
+    expect(() => assertsPerson(null)).toThrowError()
+})
