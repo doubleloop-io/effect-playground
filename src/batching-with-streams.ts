c5483c4ba751ff91738cbb389d935197c27f6960
Cosimo Matteini <dev.matteini@gmail.com>
2024-10-22 00:07:10 +0200

    batching with streams - make it work with tim smart suggestions

--- src/batching-with-streams.ts
+++ src/batching-with-streams.ts
@@ -6,26 +6,40 @@ import * as Request from "effect/Request"
 import * as RequestResolver from "effect/RequestResolver"
 import * as Stream from "effect/Stream"
 import * as Console from "effect/Console"
+import { dataLoader } from "@effect/experimental/RequestResolver"
 
-const saveItem = (item: string) => Effect.request(new SaveItem({ item }), SaveItemResolver)
+// Tim Smart version from discord thread https://effect.website/play#80697457d50e
 
-const processItem = (item: number) =>
+const processItem = (item: number, resolver: RequestResolver.RequestResolver<SaveItem, never>) =>
     Effect.gen(function* (_) {
         const processed = pipe(
             Array.replicate(`batch-${item}`, item),
             Array.map((x, i) => `${x}-${i + 1}`),
         )
 
-        yield* _(Effect.forEach(processed, (x) => saveItem(x), { batching: "inherit" }))
+        yield* _(
+            Effect.forEach(processed, (item) => Effect.request(new SaveItem({ item }), resolver), {
+                batching: "inherit",
+            }),
+        )
     })
 
 const program = Effect.gen(function* (_) {
+    /* NOTE:
+        dataLoader must be created once as it's a stateful resource (with a Scope)
+        ---
+        window: time frame to accumulate requests and then executes them in batch
+        maxBatchSize: maximum number of requests to batch together (replace RequestResolver.batchN)
+    */
+    const resolver = yield* _(dataLoader(SaveItemResolver, { window: 10, maxBatchSize: 5 }))
+
     yield* _(
         Stream.make(1, 2, 3, 4, 5),
-        Stream.mapEffect((item) => processItem(item)),
+        // NOTE: concurrency > 1 is needed to combine items across different batches
+        Stream.mapEffect((item) => processItem(item, resolver), { concurrency: 5 }),
         Stream.runDrain,
     )
-})
+}).pipe(Effect.scoped)
 
 class SaveItem extends Request.TaggedClass("SaveItem")<void, never, { item: string }> {}
 
@@ -35,7 +49,7 @@ const SaveItemResolver = RequestResolver.makeBatched((requests: Array.NonEmptyAr
         yield* _(Console.log(`Saving ${items}`))
         yield* _(Effect.forEach(requests, (request) => Request.completeEffect(request, Effect.void)))
     }),
-).pipe(RequestResolver.batchN(5))
+)
 
 const main = pipe(program, Effect.withRequestBatching(true))
 
