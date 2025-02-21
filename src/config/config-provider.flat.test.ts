3c26e306efb2d1d15c56ce86e5726940a6280309
Andrea Vallotti <andrea.vallotti@gmail.com>
2025-02-21 14:25:48 +0100

    config-provider: enumerateChildren

--- src/config/config-provider.flat.test.ts
+++ src/config/config-provider.flat.test.ts
@@ -3,7 +3,11 @@ import * as Config from "effect/Config"
 import * as ConfigError from "effect/ConfigError"
 import * as ConfigProvider from "effect/ConfigProvider"
 import * as ConfigProviderPathPatch from "effect/ConfigProviderPathPatch"
-import { expect, test } from "vitest"
+import * as HashMap from "effect/HashMap"
+import * as HashSet from "effect/HashSet"
+import * as Array from "effect/Array"
+import * as F from "effect/Function"
+import { describe, expect, test } from "vitest"
 
 const FlatProvider = (values: { [key in string]: string }) => {
     let invocations = 0
@@ -12,6 +16,9 @@ const FlatProvider = (values: { [key in string]: string }) => {
         key in values
             ? Effect.succeed(`${values[key]}@${++invocations}`)
             : Effect.fail(ConfigError.Unsupported([key], "Unknown config"))
+
+    const cannotHaveNestedConfigurations = () => HashSet.empty()
+
     return ConfigProvider.fromFlat(
         ConfigProvider.makeFlat({
             load: (path, config, split) =>
@@ -19,7 +26,15 @@ const FlatProvider = (values: { [key in string]: string }) => {
                     Effect.flatMap(config.parse),
                     Effect.map((x) => [x]),
                 ),
-            enumerateChildren: (path) => Effect.fail(ConfigError.Unsupported([...path], "TBI")),
+            enumerateChildren: (path) =>
+                F.pipe(
+                    path,
+                    Array.match({
+                        onEmpty: () => HashSet.make(...Object.keys(values)),
+                        onNonEmpty: cannotHaveNestedConfigurations,
+                    }),
+                    Effect.succeed,
+                ),
             patch: ConfigProviderPathPatch.empty,
         }),
     )
@@ -49,4 +64,29 @@ test("resolve config does not cache", async () => {
     )
 
     expect(result).toEqual(["test value@1", "test value@2"])
+})
+
+describe("enumerateChildren", () => {
+    test("non empty path with non nested field", async () => {
+        const configProvider = ConfigProvider.fromJson({ BAR: "BAR VALUE", FOO: "FOO VALUE" })
+
+        const result = await Config.hashMap(Config.string(), "FOO").pipe(
+            Effect.withConfigProvider(configProvider),
+            Effect.runPromise,
+        )
+
+        expect(result).toEqual(HashMap.empty())
+    })
+
+    test("empty path", async () => {
+        const configProvider = FlatProvider({ BAR: "BAR VALUE", FOO: "FOO VALUE" })
+        const result = await Config.hashMap(Config.string()).pipe(
+            Effect.withConfigProvider(configProvider),
+            Effect.runPromise,
+        )
+
+        expect(result).toEqual(
+            HashMap.make(["FOO", expect.stringMatching("FOO VALUE")], ["BAR", expect.stringMatching("BAR VALUE")]),
+        )
+    })
 })
